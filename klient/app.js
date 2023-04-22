aa6d18ba14f34f561294d0fe0523ef11d1c51cee
Tryniu88 <127084934+Tryniu88@users.noreply.github.com>
2023-04-22 21:34:20 +0200

    upd

--- klient/app.js
+++ klient/app.js
@@ -2,7 +2,7 @@ async function getuser() {
   document.getElementById("user").value;
   document.getElementById("pass").value;
 
-  const data = await fetch(`https://localhost/login/user/pass`);
+  const data = await fetch(`https://localhost3000/login/user/pass`);
   const json = await data.json();
   console.log(json);
 
