ee39254d194d467ba8a38c8005b67ce6acae2f90
Tryniu88 <127084934+Tryniu88@users.noreply.github.com>
2023-04-22 18:05:12 +0200

    upd

---
+++ app.js
@@ -0,0 +1,30 @@
+async function getuser() {
+  //POBIERANIE WARTOSCI Z INPUTA
+  const user = document.getElementById("user").value;
+  const pass = document.getElementById("pass").value;
+
+  //FETCH
+  const data = await fetch(`http://localhost:3000/login/${user}/${pass}`);
+  const json = await data.json();
+  console.log(json);
+
+  //LOCALSTORAGE (SPRAWDZA DOSTĘP DO UPRAWNIEŃ)
+  if (json[0] != undefined) {
+    localStorage.setItem("upr", JSON.stringify(json[0]));
+  } else {
+    localSt;
+    orage.setItem("upr", "false");
+  }
+}
+
+function checkuser() {
+  //ZAPIS JSON JAKO STRING
+  const user = JSON.parse(localStorage.getItem("upr"));
+  //PRZEKIEROWYWUJE DO STRONY STARTOWEJ
+  const url = window.location.href;
+  //--||--
+  //BEZ user.upr!!
+  if (user != "admin" && url.includes("admin.html")) {
+    window.location.href = "index.html";
+  }
+}
---
+++ config.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+const port = 3000;
+const app = express();
+
+app.use(cors());
+
+var con = mysql.createConnection({
+  database: "kartkawka",
+  user: "root",
+  password: "",
+  host: "localhost",
+});
+
+con.connec;
+t(function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected");
+  }
+});
+
+app.get("/login/:user/:pass", function (req, res) {
+  const user = req.params.user;
+  const pass = req.params.pass;
+  const sql = `SELECT upr FROM kartkawkatab WHERE user = "${user}" AND password = "${pass}"`;
+
+  con.query(sql, function (err, result, fields) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(port);
