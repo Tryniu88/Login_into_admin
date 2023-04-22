5c22af92d8bd678b1602b46d2b238c391b905478
Tryniu88 <127084934+Tryniu88@users.noreply.github.com>
2023-04-22 20:08:45 +0200

    Update app.js

--- klient/app.js
+++ klient/app.js
@@ -1,4 +1,23 @@
 async function getuser() {
   document.getElementById("user").value;
   document.getElementById("pass").value;
+
+  const data = await fetch(`https://localhost/login/user/pass`);
+  const json = await data.json();
+  console.log(json);
+
+  if (json[0] != undefined) {
+    localStorage.setItem("upr", JSON.stringify(json[0]));
+  } else {
+    localStorage.setItem("upr", "false");
+  }
+}
+
+function checkuser() {
+  const user = JSON.parse(localStorage.getItem("upr"));
+  const url = window.location.href;
+
+  if (user != "admin" && url.includes("admin.html")) {
+    window.location.href = "index.html";
+  }
 }
